4196522b0af05038b7692b42a15a622bfffe6f7f
FelipeMantilla <felipemantilla@outlook.es>
2019-12-30 11:56:05 -0500

    app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log('hola mundo')
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req,res)=>{
+    res.send('hola mundo ')
+})
+
+app.listen(3000, ()=>{
+    console.log('server on port 3000')
+})
